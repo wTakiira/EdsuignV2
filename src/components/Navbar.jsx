a557b3b1c396e98ba9f0a036c416724999794219
Eric Mai <eric.mai@my-digital-school.org>
2025-04-23 22:26:13 +0200

    WIP

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
-import FeatherIcon from 'feather-icons-react';
-
+import FeatherIcon from "feather-icons-react";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <nav className="absolute left-[20px] h-full bg-[var(--primary)]">
-      <div>
-      <FeatherIcon icon="home" />
-      </div>
+      <Link to="/">
+        <FeatherIcon icon="home" />
+      </Link>
+      <Link to="/form-inter">
+        <FeatherIcon icon="smile" />
+      </Link>
     </nav>
   );
 }
