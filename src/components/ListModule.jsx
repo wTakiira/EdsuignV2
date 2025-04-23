22053ea77756f7bdf34f52a4845359d5c4673c30
lilipey <lisa.peyron@my-digital-school.org>
2025-04-23 23:44:57 +0200

    debut style nav + list module

--- src/components/ListModule.jsx
+++ src/components/ListModule.jsx
@@ -4,9 +4,8 @@ export default function ListModule({ children }) {
   const data = ["01/01/2023", "02/01/2023", "03/01/2023", "04/01/2023"];
 
   const ElementModule = (date) => {
-    console.log(date);
     return (
-      <li style={{}}>
+      <li className='p-[20px] rounded-[var(--custom-big-radius)] bg-[var(--secondary-bis)] gap-[20px]'>
           {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             width="28"
@@ -25,25 +24,17 @@ export default function ListModule({ children }) {
               />
             </g>
           </svg> */}
-          <FeatherIcon icon="clock" />
+          <FeatherIcon icon="clock" color='var(--black)'size='32px'/>
         <p>{date.date}</p>
       </li>
     );
   };
 
   return (
-    <ul
-      className=""
-      style={{
-        width: 170,
-        height: "100%",
-        backgroundColor: "var(--secondary)",
-      }}
-    >
-      {data.map((item, index) => (
-        <ElementModule key={index} date={item} />
-      ))}
-      {/* {children} */}
-    </ul>
+    <ul className="flex flex-col gap-4 w-[170px] h-full bg-[var(--secondary)] p-[20px] rounded-[var(--custom-big-radius)] m-[var(--small-space)]">
+    {data.map((item, index) => (
+      <ElementModule key={index} date={item} />
+    ))}
+  </ul>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <nav className="absolute left-[20px] h-full bg-[var(--primary)]">
+    <nav className="absolute left-[20px] px-[24px] py-[44px] h-full bg-[var(--primary)] rounded-[var(--custom-big-radius)] flex flex-col gap-[24px] my-[var(--small-space)]">
       <Link to="/">
-        <FeatherIcon icon="home" />
+        <FeatherIcon icon="home" color='var(--secondary)'size='32px'/>
       </Link>
       <Link to="/form-inter">
-        <FeatherIcon icon="smile" />
+        <FeatherIcon icon="smile" color='var(--secondary)'size='32px'/>
       </Link>
     </nav>
   );
